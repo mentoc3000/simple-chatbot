85e5babd151c0ca92881e2b3e39f85bc2752f8b9
JP Sheehan <jp.sheehan2@gmail.com>
2024-09-17 20:56:32 -0400

    Remove on resume

--- app.py
+++ app.py
@@ -38,21 +38,6 @@ async def on_chat_start():
     setup_runnable()
 
 
-@cl.on_chat_resume
-async def on_chat_resume(thread: ThreadDict):
-    memory = ConversationBufferMemory(return_messages=True)
-    root_messages = [m for m in thread["steps"] if m["parentId"] is None]
-    for message in root_messages:
-        if message["type"] == "user_message":
-            memory.chat_memory.add_user_message(message["output"])
-        else:
-            memory.chat_memory.add_ai_message(message["output"])
-
-    cl.user_session.set("memory", memory)
-
-    setup_runnable()
-
-
 @cl.on_message
 async def on_message(message: cl.Message):
     memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
